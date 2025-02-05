90742d50cb21ea3c44f63f703be7ebb1d3c76711
Falkebr <falkebr@stud.ntnu.no>
2025-02-05 14:42:36 +0100

    replaced a bunch of stuff

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-from typing import Optional
+import openmeteo_requests
 
-from fastapi import FastAPI
+import requests_cache
+import pandas as pd
+from retry_requests import retry
 
-app = FastAPI()
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
 
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+	"latitude": 59.9127,
+	"longitude": 10.7461
+}
+responses = openmeteo.weather_api(url, params=params)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
